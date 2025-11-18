22d3d6ae8233808d3d9d294c27dd00db2abc790b
Luis <luisagp2005@gmail.com>
2025-11-17 22:59:33 -0600

    version restaurada

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,21 +1,30 @@
 <?php return array(
     'root' => array(
-        'name' => 'can/da',
+        'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b5e9e303fc92acf3aa174fdab7d1170ff873f452',
+        'reference' => 'f4469eafdf4411dc1e6c8e8e988a18d40ae4c84a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'can/da' => array(
+        '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b5e9e303fc92acf3aa174fdab7d1170ff873f452',
+            'reference' => 'f4469eafdf4411dc1e6c8e8e988a18d40ae4c84a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.11.1',
+            'version' => '6.11.1.0',
+            'reference' => 'd1e91ecf8c598d073d0995afa8cd5c75c6e19e66',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
